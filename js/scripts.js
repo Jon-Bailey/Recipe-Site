3ccca2942349841752e8a3be1fad57723b6a2889
Jon Bailey <jonbailey788@gmail.com>
2020-09-25 18:03:58 -0500

    media queries start

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ $("#signupButton").on("click", function () {
     $("#signupModal").modal("show");
   });
 });
+
+$(function() {
+  $("#signupButton2").on("click", function () {
+      $("#signupModal").modal("show");
+    });
+  });
